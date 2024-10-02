4d89583167de2409c25b68524c8a0087442b7226
ehddls98 <icbku1357@naver.com>
2024-10-02 11:28:10 +0900

    init

---
+++ src/test/java/com/finalProject/Back/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalProject.Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
