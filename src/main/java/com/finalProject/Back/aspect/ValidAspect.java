fd5aef0e5039c327564c176c8fa8944f8e80f60b
hyeon0907 <kimjh597@naver.com>
2024-10-08 11:26:10 +0900

    24-10-08 Env: "aop 구현" validAop 구현

---
+++ src/main/java/com/finalProject/Back/aspect/ValidAspect.java
@@ -0,0 +1,28 @@
+package com.finalProject.Back.aspect;
+
+import org.apache.tomcat.jni.Proc;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+
+public class ValidAspect {
+
+    @Pointcut("@annotation(com.finalProject.Back.aspect.annotation.ValidAop)")
+    private void pointCut() {}
+
+    @Around("pointCut()")
+    public Object around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object[] args = proceedingJoinPoint.getArgs();
+        BindingResult bindingResult = null;
+
+        for(Object arg : args) {
+            if(arg.getClass() == BeanPropertyBindingResult.class) {
+                bindingResult = (BeanPropertyBindingResult) arg;
+            }
+        }
+
+        return proceedingJoinPoint.proceed();
+    }
+}
