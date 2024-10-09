b8caacba69bbb7e9af29ff4013b6e5b2a14c178f
ehddls98 <icbku1357@naver.com>
2024-10-09 22:58:37 +0900

    Feat: 게시글 작성 기능 구현 게시글 작성 기능을 구현하기 위해controller, service, mapper, entity, dto를 작성하였음.

--- src/main/java/com/finalProject/Back/config/SecurityConfig.java
+++ src/main/java/com/finalProject/Back/config/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.cors();
         http.authorizeRequests()
-                .antMatchers("/")
+                .antMatchers("/board/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated();
