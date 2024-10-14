f7e1a412140084741a8918b63f7a17a0de2268f7
CodeDDAGGI <qudrb1132@naver.com>
2024-10-14 11:30:35 +0900

    Feat : back 로그인 작업

--- src/main/java/com/finalProject/Back/dto/request/Owner/ReqOwnerSigninDto.java
+++ src/main/java/com/finalProject/Back/dto/request/Owner/ReqOwnerSigninDto.java
@@ -1,13 +1,14 @@
-package com.finalProject.Back.dto.request.Owner;
-
-import lombok.Data;
-
-import javax.validation.constraints.NotBlank;
-
-@Data
-public class ReqOwnerSigninDto {
-    @NotBlank(message = "사용자 이름을 입력해주세요")
-    private String username;
-    @NotBlank(message = "비밀번호를 입력해주세요")
-    private String password;
-}
+//package com.finalProject.Back.dto.request.Owner;
+//
+//import lombok.Data;
+//
+//import javax.validation.constraints.NotBlank;
+//
+//@Data
+//public class ReqOwnerSigninDto {
+//    @NotBlank(message = "사용자 이름을 입력해주세요")
+//    private String username;
+//    @NotBlank(message = "비밀번호를 입력해주세요")
+//    private String password;
+//}
+// 미사용
