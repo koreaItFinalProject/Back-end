c3411dd3ac69280d569226e1c17cc9a8962ddd89
CodeDDAGGI <qudrb1132@naver.com>
2024-10-14 17:51:25 +0900

    Feat : "로그인 회원가입 완료"

--- src/main/java/com/finalProject/Back/dto/request/User/ReqSignupDto.java
+++ src/main/java/com/finalProject/Back/dto/request/User/ReqSignupDto.java
@@ -11,18 +11,24 @@ import javax.validation.constraints.Pattern;
 @Data
 public class ReqSignupDto {
 
+    @NotBlank
     @Pattern(regexp = "^[a-z0-9]{8,}$", message = "사용자이름은 8자이상의 영소문자 , 숫자 조합이여야합니다.")
     private String username;
+    @NotBlank
     @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[~!@#$%^&*?])[A-Za-z\\d~!@#$%^&*?]{8,16}$", message = "비밀번호는 8자이상 16자 이하의 영대소문, 숫자, 특수문자(~!@#$%^&*?)를 포함해야 합니다.")
     private String password;
+    @NotBlank(message = "비밀번호 공백일 수 없습니다")
     private String checkPassword;
+    @NotBlank
     @Pattern(regexp = "^[가-힣]+$", message = "한글로 된 이름을 기입해주세요.")
     private String name;
     @NotBlank(message = "이메일은 공백일 수 없습니다.")
     @Email(message = "이메일 형식이어야 합니다")
     private String email;
+    @NotBlank
     @Pattern(regexp = "^.{1,10}$", message = "닉네임은 10글자 이내여야 하고 공백일 수 없습니다.")
     private String nickname;
+    private String role;
 
     public User toEntity(BCryptPasswordEncoder passwordEncoder) {
         return User.builder()
@@ -31,6 +37,7 @@ public class ReqSignupDto {
                 .name(name)
                 .nickname(nickname)
                 .email(email)
+                .role(role)
                 .build();
     }
 }
