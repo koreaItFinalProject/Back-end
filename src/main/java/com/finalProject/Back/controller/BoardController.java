72bbf7442f378e8d2d81e3b3f74619c895f359b7
hyeon0907 <kimjh597@naver.com>
2024-10-15 18:00:27 +0900

    24-10-15 Fix:"comment.xml 수정" comment.xml 수정

--- src/main/java/com/finalProject/Back/controller/BoardController.java
+++ src/main/java/com/finalProject/Back/controller/BoardController.java
@@ -1,10 +1,13 @@
 package com.finalProject.Back.controller;
 
 import com.finalProject.Back.dto.request.ReqBoardDto;
+import com.finalProject.Back.entity.Comment;
 import com.finalProject.Back.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class BoardController {
--- src/main/java/com/finalProject/Back/controller/CommentController.java
+++ src/main/java/com/finalProject/Back/controller/CommentController.java
@@ -1,11 +1,16 @@
 package com.finalProject.Back.controller;
 
 import com.finalProject.Back.dto.request.ReqCommentDto;
+import com.finalProject.Back.entity.Comment;
 import com.finalProject.Back.service.CommentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Slf4j
 @RestController
 public class CommentController {
 
@@ -14,12 +19,15 @@ public class CommentController {
 
     @PostMapping("/comment")
     public ResponseEntity<?> write(@RequestBody ReqCommentDto.WriteDto dto) {
+        System.out.println(dto);
         commentService.write(dto);
         return ResponseEntity.ok().body(true);
     }
 
     @GetMapping("/comment/{boardId}")
     public ResponseEntity<?> getAll(@PathVariable Long boardId) {
+        List<Comment> commentList = commentService.getAll(boardId).getComments();
+        log.info("{}", commentList);
         return ResponseEntity.ok().body(commentService.getAll(boardId));
     }
 
