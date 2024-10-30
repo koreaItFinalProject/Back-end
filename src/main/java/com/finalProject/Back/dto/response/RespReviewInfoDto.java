69386741d72a2b09a16df4105e6d129c0cdb8076
CodeDDAGGI <qudrb1132@naver.com>
2024-10-30 15:13:31 +0900

    Feat : "디자인 수정"

--- src/main/java/com/finalProject/Back/dto/response/RespReviewInfoDto.java
+++ src/main/java/com/finalProject/Back/dto/response/RespReviewInfoDto.java
@@ -10,6 +10,9 @@ import java.time.LocalDate;
 public class RespReviewInfoDto {
     private Long cafeId;
     private String cafeName;
+    private String category;
+    private String img;
+    private Long writerId;
     private int rating;
     private String review;
     private String writeDate;
