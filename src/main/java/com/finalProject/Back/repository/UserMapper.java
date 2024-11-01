e54648aa7d9fd4526637250b2580d70c38ce1c6b
CodeDDAGGI <qudrb1132@naver.com>
2024-11-01 12:21:57 +0900

    Feat : "중복 체크 확인"

--- src/main/java/com/finalProject/Back/repository/UserMapper.java
+++ src/main/java/com/finalProject/Back/repository/UserMapper.java
@@ -4,6 +4,7 @@ import com.finalProject.Back.dto.response.RespInfoDto;
 import com.finalProject.Back.dto.response.RespUserInfoDto;
 import com.finalProject.Back.entity.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface UserMapper {
@@ -13,8 +14,12 @@ public interface UserMapper {
     User findByOAuth2Name(String username);
     User findById(Long id);
     RespUserInfoDto findUserInfoById(Long id);
+    int findDuplicatedValue(@Param("fieldName") String fieldName, @Param("value") String value);
+    int updateFieldValue(@Param("id") Long id, @Param("fieldName") String fieldName, @Param("value") String value);
     boolean existsByUsername(String username);
     boolean existsByNickname(String nickname);
+
     int update(User user);
     int imgUpdate(User user);
+
 }
